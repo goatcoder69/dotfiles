4c7c910a4ab5c073aa76597eed257f83903e7d04
Srinath <60451672+goatcoder69@users.noreply.github.com>
2024-05-23 19:34:27 -0400

    add rose-pine package for styling

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -15,6 +15,14 @@ require("lazy").setup("plugins")
 
 return {
   { "nvim-treesitter/nvim-treesitter", build=":TSUpdate" },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      -- require("rose-pine.config")
+      vim.cmd("colorscheme rose-pine")
+    end
+  },
   -- https://github.com/elbywan/crystalline?tab=readme-ov-file#vimneovim
   { "neoclide/coc.nvim", branch="release" },
   "vim-crystal/vim-crystal"
